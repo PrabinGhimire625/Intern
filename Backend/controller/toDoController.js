bdfa03984318ee2b630a2321871eedcb791250c0
PrabinGhimire625 <prabinghimire625@gmail.com>
2025-07-10 11:42:10 +0545

    search functionality

--- Backend/controller/toDoController.js
+++ Backend/controller/toDoController.js
@@ -1,12 +1,9 @@
 import ToDo from "../model/todoModel.js";
 
-
-//create todo API
+//create todo
 export const createTodo = async (req, res) => {
     try {
-        const { title, description } = req.body;  //postman  ....frontend
-
-        //status code 
+        const { title, description } = req.body;  
         if (!title || !description) {
             return res.status(400).json({ message: "Title and the description must required" });
         }
@@ -14,7 +11,6 @@ export const createTodo = async (req, res) => {
         if (existingTodo) {
             return res.status(404).json({ message: "Title already exist" });
         }
-
         const todo = await ToDo.create({
             title, description
         });
@@ -22,15 +18,14 @@ export const createTodo = async (req, res) => {
 
     } catch (err) {
         res.status(500).json({ error: "Internal server error" });
-
     }
 }
 
 
-//get/read todo
+// get all todo
 export const getTodo = async (req, res) => {
     try {
-        const todo = await ToDo.find();
+        const todo = await ToDo.find().sort({ createdAt: -1 });
         res.status(200).json({ message: "Todo created successfully", data: todo });
     } catch (err) {
         res.status(500).json({ error: "Internal server error" });
@@ -53,11 +48,11 @@ export const singleTodo = async (req, res) => {
 }
 
 
-//update  API
+//update  todo
 export const updateTodo = async (req, res) => {
     try {
         const { id } = req.params;
-        const { title, description } = req.body;  //postman  ....frontend
+        const { title, description } = req.body; 
 
         //status code 
         if (!title || !description) {
@@ -70,6 +65,8 @@ export const updateTodo = async (req, res) => {
         }
 
         const todo = await ToDo.findByIdAndUpdate(id, req.body, { new: true });
+        //const todo = await ToDo.findByIdAndUpdate(id, { title, description }, { new: true });
+
 
         res.status(200).json({ message: "Todo updated successfully", data: todo })
 
@@ -88,7 +85,7 @@ export const deleteTodo = async (req, res) => {
         }
         const todo = await ToDo.findByIdAndDelete(id);
         if(!todo){
-            return res.status(400).json({ message: "Todo not found" });
+            return res.status(404).json({ message: "Todo not found" });
         }
         res.status(200).json({ message: "Todo deleted successfully"})
     } catch (err) {
@@ -97,7 +94,29 @@ export const deleteTodo = async (req, res) => {
     }
 }
 
+// search the todo
+export const searchTodo= async(req, res)=>{
+    try{
+        const {query}=req.query;
+        console.log("Query : ", query)
+        if(!query){
+           return res.status(400).json({message : "Query must be required"})
+        }
 
+        const todos= await ToDo.find({
+            $or: [
+                {title :  {$regex : query, $options : "i"}},
+                {description :  {$regex : query, $options : "i"}},
+            ]
+        });
 
+        if(todos.length===0){
+            return res.status(404).json({message : "To do not found on this search"});
+        }
 
+        res.status(200).json({message : "Search result ", data : todos})
+    }catch(err){
+        res.status(500).json({error : "Internal server error"});
 
+    }
+}
